827f395ac63553cde9a692baf2c06d195f4f10a3
suna <choisuna123@naver.com>
2023-02-27 21:25:47 +0900

    main page

--- src/main/java/com/ss/Jamong/MainController.java
+++ src/main/java/com/ss/Jamong/MainController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String main(){
-        return "main";
+        return "main/main";
     }
 }
