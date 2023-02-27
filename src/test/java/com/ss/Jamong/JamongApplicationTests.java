0ac749784de33c579ffef958c22e2b5466e9a923
suna <choisuna123@naver.com>
2023-02-27 20:33:30 +0900

    spring init

---
+++ src/test/java/com/ss/Jamong/JamongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.Jamong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
