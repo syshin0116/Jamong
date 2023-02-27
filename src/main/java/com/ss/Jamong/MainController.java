a3b0734fc60b3091c632811bb531ca8fb9ccd46c
suna <choisuna123@naver.com>
2023-02-27 20:41:37 +0900

    MainController

--- src/main/java/com/ss/Jamong/MainController.java
+++ src/main/java/com/ss/Jamong/MainController.java
@@ -1,4 +1,14 @@
 package com.ss.Jamong;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
 }
