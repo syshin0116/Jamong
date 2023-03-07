18bdf24464373519d5274235805ae443beaece52
syshin0116 <syshin0116@gmail.com>
2023-03-07 19:43:48 +0900

    0305 oauth2 authorization problem, register

--- src/main/java/com/ss/Jamong/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/ss/Jamong/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -55,7 +55,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         } catch (Exception e) {
             throw e;
         }
-
     }
 
     // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
@@ -67,5 +66,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getUsername(), refreshToken);
+        log.info("loginSuccess!! AccessToken:"+accessToken);
+        log.info("loginSuccess!! RefreshToken:"+refreshToken);
     }
 }
