c2e800f670e208462ad9df93acb0062ea13c33ab
syshin0116 <syshin0116@gmail.com>
2023-03-12 20:41:17 +0900

    0312 jwtlogin deprecated config fix

--- src/main/java/com/ss/Jamong/config/SecurityConfig.java
+++ src/main/java/com/ss/Jamong/config/SecurityConfig.java
@@ -59,12 +59,10 @@ public class SecurityConfig {
                 .and()
 
                 //== URL별 권한 관리 옵션 ==//
-                .authorizeRequests()
-
+                .authorizeHttpRequests()
 
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
-
                 .requestMatchers("/", "/assets/**", "/css/**", "/js/**", "/image/**", "/login", "/register", "/user/**" ,"/oauth2Register").permitAll()
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .and()
