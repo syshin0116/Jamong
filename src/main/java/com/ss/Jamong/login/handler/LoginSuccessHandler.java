393f830463d397c4fa04a1e9512e038bd71507ec
syshin0116 <syshin0116@gmail.com>
2023-03-13 15:02:30 +0900

    0312 jwtlogin authorization header not maintained

--- src/main/java/com/ss/Jamong/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/ss/Jamong/login/handler/LoginSuccessHandler.java
@@ -25,6 +25,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
         String username = extractUsername(authentication); // 인증 정보에서 Username(username) 추출
+//        String nickname = extractNickname(authentication);
         String accessToken = jwtService.createAccessToken(username); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
         String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
 
@@ -39,6 +40,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
         log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
     }
+
 
     private String extractUsername(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
